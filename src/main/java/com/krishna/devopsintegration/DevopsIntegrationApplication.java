7e04bc8c1b0196aca56efa26c7a5fb4242a923ea
teja2074 <mthokaram@gmail.com>
2023-02-24 14:41:18 +0000

    End point added

--- src/main/java/com/krishna/devopsintegration/DevopsIntegrationApplication.java
+++ src/main/java/com/krishna/devopsintegration/DevopsIntegrationApplication.java
@@ -2,11 +2,20 @@ package com.krishna.devopsintegration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DevopsIntegrationApplication {
 
+	@GetMapping("/welcome")
+	public String welcome(){
+		return "Welcome to CI/CD pipelines";
+	}
+
 	public static void main(String[] args) {
+
 		SpringApplication.run(DevopsIntegrationApplication.class, args);
 	}
 
